14d6151f2be8ec18ec85642ed7fed39babd6f89e
Ltstriker <ltuse163@163.com>
2017-12-05 13:51:47 +0800

    final

--- calculator.js
+++ calculator.js
@@ -1,17 +1,111 @@
-/**
- * just a test
- */
+
+
+
+
+
+
+
+
 var tal="0";
 var arr1=["+","-","*","/"];
 var arr2=["1","2","3","4","5","6","7","8","9","0"];
 
-var err=1;
+var err=0;
+
+window.onload = function()
+{
+	document.getElementById("button1").onclick = function(){get('1');};
+	document.getElementById("button2").onclick = function(){get('2');};
+	document.getElementById("button3").onclick = function(){get('3');};
+	document.getElementById("button4").onclick = function(){get('/');};
+	document.getElementById("button5").onclick = function(){get('4');};
+	document.getElementById("button6").onclick = function(){get('5');};
+	document.getElementById("button7").onclick = function(){get('6');};
+	document.getElementById("button8").onclick = function(){get('*');};
+	document.getElementById("button9").onclick = function(){get('7');};
+	document.getElementById("button10").onclick = function(){get('8');};
+	document.getElementById("button11").onclick = function(){get('9');};
+	document.getElementById("button12").onclick = function(){get('-');};
+	document.getElementById("button13").onclick = function(){get('0');};
+	document.getElementById("button14").onclick = function(){get('.');};
+	document.getElementById("button15").onclick = function(){del();};
+	document.getElementById("button16").onclick = function(){get('+');};
+	document.getElementById("button17").onclick = function(){get('(');};
+	document.getElementById("button18").onclick = function(){get(')');};
+	document.getElementById("button19").onclick = function(){CE();};
+	document.getElementById("button20").onclick = function(){calculate();};
+
+	document.onkeydown=function(event){
+		var E = event || window.event || arguments.callee.caller.arguments[0];
+		var keycode = E.keyCode;
+		if(keycode>95&&keycode<106)
+		{
+			if(event.shiftKey==true&&keycode==105)
+				get('(');
+			else if (event.shiftKey==true&&keycode==95) {
+				get(')');
+			}
+			else
+				get((keycode-96).toString());
+		}
+		else if (keycode == 13) {
+			calculate();
+		}
+		else if (keycode == 8) {
+			del();
+		}
+		else if (keycode == 110) {
+			get('.');
+		}
+		else if(keycode == 107)
+		{
+			get('+');
+		}
+		else if (keycode == 109) {
+			get('-');
+		}
+		else if (keycode == 106) {
+			get('*');
+		}
+		else if (keycode == 111) {
+			get('/');
+		}
+	};
+}
+
+
+
+function check_1()
+{
+	for(var c1 = tal.length-1;c1>=0;c1--)
+	{
+		if(tal[c1]==".")
+			return false;
+		for(var c2= 0;c2<10;c2++)
+		{
+			if(tal[c1]=='+'||tal[c1]=='-'||tal[c1]=='/'||tal[c1]=='*'||tal[c1]=='('||tal[c1]==')')
+				return true;
+		}
+	}
+	return true;
+}
 
 function get(var1)
 {
-	if(tal!="0"||var1=='.')
-		tal=tal.concat(var1);
-	else
+	if(tal.length>=21)
+		window.alert("太长了，算不过来~~~");
+	if(tal!="0"||var1=='.'||inArray(var1,arr1))
+	{
+		if(var1=='.'&&!check_1())
+		{
+			return;
+		}
+		else
+		{
+			tal=tal.concat(var1);
+		}
+	}
+	else if(var1!= ')')
 		tal=var1;
 	document.getElementById("demo").innerHTML=tal;
 }
@@ -35,8 +129,8 @@ function varify1()
 		if(tal[c1] == '(')
 		{
 			count++;
-			if((c1==0 || inArray(tal[c1-1],arr1) || tal[c1-1] == "(" ) && (c1!=tal.length-1&& inArray(tal[c1+1],arr2)))
-				;
+			if((c1==0 || inArray(tal[c1-1],arr1) || tal[c1-1] == "(" ) && (c1!=tal.length-1&& (inArray(tal[c1+1],arr2)||tal[c1+1]=='('||tal[c1+1]=='-')))
+				var _nothing=0;
 			else {
 				return false;
 			}
@@ -122,10 +216,13 @@ function cal2(temp)//get all * and / out
 	var result=1;
 	var pointer=0;
 
+	var mi = 0;
+
 	if(get_pointOfcc(temp,pointer)==-1)
-		return temp;
+		return (parseFloat(temp)*100000).toString();
 	else {
-		result*=parseFloat(temp.substring(pointer,get_pointOfcc(temp,pointer)));
+		result*=parseFloat(temp.substring(pointer,get_pointOfcc(temp,pointer)))*10000;
+		mi++;
 		pointer=get_pointOfcc(temp,pointer)+1;
 	}
 
@@ -135,12 +232,16 @@ function cal2(temp)//get all * and / out
 		if(temp_var==-1)
 		{
 			if(temp[pointer-1]=="*"){
-				result*=parseFloat(temp.substring(pointer));
+				result*=parseFloat(temp.substring(pointer))*10000;
+				mi++;
 			}
 			else {
 				var s_temp=parseFloat(temp.substring(pointer));
 				if(s_temp!=0)
-					result/=parseFloat(temp.substring(pointer));
+				{
+					result/=parseFloat(temp.substring(pointer))*10000;
+					mi--;
+				}
 				else {
 					window.alert("You could not make '0' as a divisor!");
 					err=1;
@@ -150,12 +251,15 @@ function cal2(temp)//get all * and / out
 		}
 		else {
 			if(temp[pointer-1]=="*"){
-				result*=parseFloat(temp.substring(pointer,temp_var));
+				result*=parseFloat(temp.substring(pointer,temp_var))*10000;
+				mi++;
 			}
 			else {
 				var s_temp=parseFloat(temp.substring(pointer,temp_var));
 				if(s_temp!=0)
-					result/=s_temp;
+				{
+					result/=s_temp*10000;
+				}
 				else {
 					window.alert("You could not make '0' as a divisor!");
 					err=1;
@@ -164,7 +268,24 @@ function cal2(temp)//get all * and / out
 			pointer=temp_var+1;
 		}
 	}
-	return result.toString();
+
+	if(mi>0)
+	{
+		while(mi>0)
+		{
+			result/=10000;
+			mi--;
+		}
+	}
+	else if(mi<0)
+	{
+		while (mi<0) {
+			result*=10000;
+			mi++;
+		}
+	}
+
+	return (result*100000).toString();
 }
 
 
@@ -243,13 +364,18 @@ function cal(temp1)
 		}
 	}
 
-	return sum.toString();
+	return (sum/100000).toString();
 }
 
 function calculate()
 {
 	if(varify1() && varify2()){
 		tal=cal(tal);
+		if(err==1)
+		{
+			err =0;
+			tal='0';
+		}
 	}
 	else {
 		window.alert("wrong format");
